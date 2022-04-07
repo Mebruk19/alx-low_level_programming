9eab760bf0f43d3b792943dd3538a08fe882334d
Mebruk19 <abdumebruk21@gmail.com>
2022-04-07 23:46:46 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /**
  * main - entry point
  *
- * Return: 0 after printing the function
+ * Return: 0 after printing function
  */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	prints("/"Programming is like building a multilingual puzzle");
 	return (0);
 }
