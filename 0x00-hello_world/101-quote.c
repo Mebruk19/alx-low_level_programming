836a8801337b1d215e354a53d666f2dcaed01986
Mebrul19 <abdumebruk21@gmail.com>
2022-04-09 02:29:46 +0300

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <unisted.h>
+#include <unistd.h>
 
 /**
- * main -Entry point
+ *main - Entry point 
  *
- * return: Always 0 (Success)
+ *Return: Always 0 (success)
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is usefull\" - Dora Koopar, 2015-10-19\n", 59);
 return (1);
 }
