f887dc60aeccc1a0e123b9d0906f320badaaab1c
Mebruk19 <abdumebruk21@gmail.com>
2022-04-10 12:17:05 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  *
- * Description: Write a program that prints the numbers from 00 to 99
+ * Description: Print the lowercase alphabet
  * Return: 0
  */
 int main(void)
 {
-  int digits[2] = { 0, 0 };
-  int sep[2] = { ',', ' ' };
-  int i = 0;
+  int c;
 
-  for (digits[0] = 0; digits[0] < 10; ++(digits[0]))
-    {
-      for (digits[1] = 0; digits[1] < 10; ++(digits[1]))
-	{
-	  for (i = 0; i < 2; ++i)
-	    putchar('0' + digits[i]);
-
-	  if (digits[0] != 9 || digits[1] != 9)
-	    {
-	      for (i = 0; i < 2; ++i)
-		putchar(sep[i]);
-	    }
-	}
-    }
+  for (c = 0; c < 26; ++c)
+    putchar('a' + c);
 
   putchar('\n');
 
