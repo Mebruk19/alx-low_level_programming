1c460182abfc08cd490c1f09bce2e4e7ac747cf5
Mebruk19 <abdumebruk21@gmail.com>
2022-04-10 13:33:17 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Prints the numbers since 0 to 9
  *
- * Description: Print numbers 0 to 9 on one line
- * Return: 0
+ * Return: Always (Success)
  */
 int main(void)
 {
-char i;
+int n;
 
-for (i = '0'; i <= '9'; ++i)
-putchar(i);
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
 
 putchar('\n');
 
