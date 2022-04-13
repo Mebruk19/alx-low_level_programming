63b06d4b3bc56c788336bb6492569d082271cf6d
Mebrul19 <abdumebruk21@gmail.com>
2022-04-13 05:13:13 +0300

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,26 @@
-#include <limits.h>
-#include <math.h>
 #include <stdio.h>
-
 /**
- * main - entry point
- *
- * Return: Always 0.
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
  */
 int main(void)
 {
-int i;
-int j;
-long a_lo = 1;
-long b_lo = 2;
-long a_hi = 0;
-long b_hi = 0;
-int limit_len = floor(log10(LONG_MAX / 2));
-long limit = pow(10, limit_len);
+int i = 0;
+unsigned long int a = 0, b = 1, next = 0;
 
-for (i = 0; i < 98; ++i)
+while (i < 98)
 {
-if (a_hi)
-{
-printf("%ld", a_hi);
-for (j = floor(log10(a_lo)) + 1; j < limit_len; ++j)
-putchar('0');
-}
-printf("%ld", a_lo);
-b_lo = b_lo + a_lo;
-a_lo = b_lo - a_lo;
-a_hi = b_hi - a_hi;
-b_hi = b_hi + a_hi;
-if (b_lo >= limit)
-{
-b_hi += b_lo / limit;
-b_lo %= limit;
-a_hi += a_lo / limit;
-a_lo %= limit;
-}
+next = a + b;
+a = b;
+b = next;
+printf("%lu", next);
+
 if (i < 97)
 printf(", ");
+i++;
 }
 putchar('\n');
-
 return (0);
 }
