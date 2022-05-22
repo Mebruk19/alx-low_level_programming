670d1bd9e92e965b56e9bb092149fe9ac484b010
Mebruk19 <101485086+Mebruk19@users.noreply.github.com>
2022-05-22 18:42:27 +0300

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,6 +9,7 @@
 /**
  * print_lists - prints all the elements of  a lit_t list.
  * @h: the list_t list.
+ *
  * return: the number of nodes in h.
  */
 
@@ -16,15 +17,17 @@ size_t rint_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (0)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 	
 		else
 			printf("[%d] %s\n", h->len, h->str);
+		
 		nodes++;
 		h = h->next;
 	}
+	
 	return (nodes);
 }
